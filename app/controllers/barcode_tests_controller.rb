5d7d34cee36b6a89ea6b4916715a45c758af8095
elanfer <elanfer@uni-osnabrueck.de>
2015-09-02 15:26:22 +0200

    added remove method for old files, to keep the dirs clean

--- app/controllers/barcode_tests_controller.rb
+++ app/controllers/barcode_tests_controller.rb
@@ -10,14 +10,25 @@ class BarcodeTestsController < ApplicationController
   # GET /barcode_tests
   # GET /barcode_tests.json
   def index
-    @prodid = '00000001'
+    @prodid = '00000002'
     @barcode = Barby::Code128B.new(@prodid)
     if(!File.exist?('public/barcodes/'+@prodid+'.png'))
       File.open('public/barcodes/'+@prodid+'.png', 'w'){|f|
         f.write @barcode.to_png(:height => 60, :margin => 5)
       }
     end
+    remove_old_barcodes
+  end
 
+  #deletes barcodes which are older 0.03days
+  def remove_old_barcodes
+    Dir.foreach('public/barcodes') do |filename|
+      next if filename == '.' or filename == '..'
+      time = (Time.now - File.stat('public/barcodes/'+filename).mtime).to_i / 86400.0
+      if(time>0.03)
+        File.delete('public/barcodes/'+filename)
+      end
+    end
   end
 
   # GET /barcode_tests/1
