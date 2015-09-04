f61172a2b1e04870787628b66216a3100cb42aef
fwieneke <fwieneke@uni-osnabrueck.de>
2015-09-04 10:09:49 +0200

    readded code for fixing double blank username bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,38 @@
 class User < ActiveRecord::Base
   before_create :default_values
   after_save :create_rights
+  before_save :nil_if_blank
 
 
-  has_one :right , dependent: :destroy
-  has_one :session ,dependent: :destroy
+  has_one :right, dependent: :destroy
+  has_one :session, dependent: :destroy
 
   has_many :lendings
   belongs_to :unit
   has_one :operation
 
-
-
-  validates :prename , presence: true
-  validates :lastname , presence: true
-  validates :username , uniqueness: true , allow_nil: true
-
-
-  validates :unit_id , presence: true
+  validates :prename, presence: true
+  validates :lastname, presence: true
+  validates :username, uniqueness: true, allow_nil: true
+  validates :unit_id, presence: true
 
 
   protected
-    def default_values
-      self.active = true
-    end
+  def default_values
+    self.active = true
+  end
 
-    def create_rights
-      if !self.username.nil?
-        if self.right.nil?
-          Right.create(:user=>self)
-        end
+  def create_rights
+    if !self.username.nil?
+      if self.right.nil?
+        Right.create(:user => self)
       end
     end
+  end
+
+  def nil_if_blank
+    if self.username.blank?
+      self.username = nil
+    end
+  end
 end
