d10f49e1ae9c555f99dcba4613234eda45284fc7
elanfer <elanfer@uni-osnabrueck.de>
2015-09-04 14:17:52 +0200

    test removed to fix deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  before_action do
-      Rack::MiniProfiler.authorize_request
-  end
+  #only for performance testing
+  #before_action do
+  #    Rack::MiniProfiler.authorize_request
+  #end
 end
