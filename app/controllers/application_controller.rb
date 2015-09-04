7437f1a050d721613bcf0e940f476155bc4b3314
ramarx <ramarx@cip89.informatik.uni-osnabrueck.de>
2015-09-04 15:47:43 +0200

    synchronized/formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  #helper_method :current_user
-
-
-
-  #private
-  #def current_user
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
+  after_validation :encrypt_password
+  after_create :default_values
+  before_save :nil_if_blank
+  after_save :create_rights
+
+
   attr_accessor :password_unhashed
+
   has_one :right , dependent: :destroy
   has_one :session ,dependent: :destroy
 
@@ -12,12 +19,9 @@ class User < ActiveRecord::Base
   validates :prename , presence: true
   validates :lastname , presence: true
   validates :username , uniqueness: true , allow_nil: true
-
-
   validates :unit_id , presence: true
 
-  after_validation :encrypt_password
-  after_create :default_values
+
 
   def self.authenticate(username, password_unhashed)
     user = find_by_username(username)
@@ -36,8 +40,22 @@ class User < ActiveRecord::Base
     end
   end
 
-  private
+  protected
     def default_values
       self.active = true
+    end
+
+    def create_rights
+      if !self.username.nil?
+        if self.right.nil?
+          Right.create(:user => self)
+        end
+      end
+    end
+
+    def nil_if_blank
+      if self.username.blank?
+        self.username = nil
+      end
     end
 end
