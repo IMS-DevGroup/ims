a363056a9800347644c5dc0dc95ca0790f4af409
Frank Wieneke <fwieneke@uos.de>
2015-09-08 15:07:38 +0200

    added language selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_filter :set_locale
+
+  #checks default browser language
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
+  #sets localisation (magic)
+  private
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
+  end
+
+
 end
