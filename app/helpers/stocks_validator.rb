275538cef8d197fbd9bcace9cbe639192992a90f
djuelich <djuelich@uni-osnabrueck.de>
2015-09-08 13:33:18 +0200

    removed unnecessary double error handlings

--- app/helpers/stocks_validator.rb
+++ app/helpers/stocks_validator.rb
@@ -1,10 +1,2 @@
 class Stocks_Validator < ActiveModel::Validator
-
-  def validate(stock)
-
-    if stock.name.blank?
-      stock.errors[:base] <<"Insert Name!"
-    end
-
-  end
 end
--- app/helpers/units_validator.rb
+++ app/helpers/units_validator.rb
@@ -1,10 +1,3 @@
 class Units_Validator < ActiveModel::Validator
-
-  def validate(unit)
-
-    if stock.name.blank?
-      stock.errors[:base] <<"Insert Name!"
-
-    end
-  end
 end
+
