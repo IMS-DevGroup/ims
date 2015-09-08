f89d639cb319185d8d5442fd0494027f536e433d
Daniel Kleine <dkleine@uos.de>
2015-09-08 09:39:16 +0200

    security update, links without login does not work anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_filter :require_login
+
+  private
+    def require_login
+      unless current_user
+        redirect_to '/login/'
+      end
+    end
+
+
 end
