65bdf7ed8fe0e27bf44b62d1dfef40972a92f1fe
ramarx <ramarx@cip89.informatik.uni-osnabrueck.de>
2015-09-08 15:03:11 +0200

    deleting unneeded method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,34 +20,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sessions/1
-  # PATCH/PUT /sessions/1.json
-  def update
-    respond_to do |format|
-      if @session.update(session_params)
-        format.html { redirect_to @session, notice: 'Session was successfully updated.' }
-        format.json { render :show, status: :ok, location: @session }
-      else
-        format.html { render :edit }
-        format.json { render json: @session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def remove
       log_out if logged_in?
       redirect_to root_url
-  end
-
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_session
-    @session = Session.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def session_params
-    params.require(:session).permit(:session_key, :remember_me)
   end
 end
