fafb0fab9b2f9a0d191986216998168f901b10f8
djuelich <djuelich@uni-osnabrueck.de>
2015-09-08 10:24:55 +0200

    added stocks_validator added error handlings

--- app/helpers/stocks_validator.rb
+++ app/helpers/stocks_validator.rb
@@ -1,7 +1,8 @@
 class Stocks_Validator < ActiveModel::Validator
+
   def validate(stock)
+
     if stock.name.blank?
-      puts "hier sollte ich hin"
       stock.errors[:base] <<"Insert Name!"
     end
 
