ff889c6d131496f98a7828c4d27ad55fb34b8da9
djuelich <djuelich@uni-osnabrueck.de>
2015-09-08 14:24:33 +0200

    removed validation of city and streets

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,6 @@ class Stock < ActiveRecord::Base
 
   validates :unit_id , presence: true
   validates :name , presence: true
-  validates :city, presence: :true
-  validates :street, presence: :true
 
 
 end
