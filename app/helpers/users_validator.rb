bd230742682738c5670e6856021ad776f08ef5f2
Frank Wieneke <fwieneke@uos.de>
2015-09-09 11:22:38 +0200

    Merge branch 'dev' of https://github.com/OsnaCS/drk-funk into dev
    # Conflicts:
    #	app/helpers/users_validator.rb

--- app/helpers/users_validator.rb
+++ app/helpers/users_validator.rb
@@ -2,13 +2,13 @@ class Users_Validator < ActiveModel::Validator
 
   def validate(user)
     if user.active?
-    #look for username and password combination
-    if !(user.password_unhashed.blank?) && user.username.blank?
-      user.errors[:base] << "If password is inserted you have to insert a username"
-    elsif !(user.username.blank?) && user.password_unhashed.blank?
-      user.errors[:base] << "If username is inserted you have to insert a password"
+      #look for username and password combination
+      if !(user.password_unhashed.blank?) && user.username.blank?
+        user.errors[:base] << "If password is inserted you have to insert a username"
+      elsif !(user.username.blank?) && user.password_unhashed.blank?
+        user.errors[:base] << "If username is inserted you have to insert a password"
+      end
     end
-
     #look for correct mobile number
     if !(user.mobile_number.blank?)
       puts "hier kommt die maus"
@@ -20,3 +20,4 @@ class Users_Validator < ActiveModel::Validator
     end
   end
 end
+
