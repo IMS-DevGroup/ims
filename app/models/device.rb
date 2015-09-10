071f90823ec4737e345e229b4c0b2dc77bd7d205
mbrockmo <mbrockmo@uos.de>
2015-09-10 12:47:25 +0200

    add language column to properties

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,7 @@
 class Device < ActiveRecord::Base
 
+
+
   has_many :lendings
   has_many :values
   belongs_to :device_type
@@ -8,6 +10,8 @@ class Device < ActiveRecord::Base
   validates :owner_id, presence: :true
   validates :ready , presence: true
   validates :stock_id, presence: :true
+
+
 
 
   def self.fill
