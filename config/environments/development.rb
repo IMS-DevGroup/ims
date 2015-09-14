9ed2bcde517b0ba868bd5734fe4a0bac105f049f
Frank Wieneke <fwieneke@uni-osnabrueck.de>
2015-09-14 14:29:22 +0200

    added mailer functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,27 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Action mailer options
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.1und1.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => "Kontakt.IMS@fw-technology.com",
+      :password => "w%[uu}A2Q77[",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
 end
