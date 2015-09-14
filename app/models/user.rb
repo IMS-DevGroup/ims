967367fbb2bb4f30f34ece37ff756390ee325143
eossovski <eossovski@uos.de>
2015-09-14 11:05:00 +0200

    added lower/upper case validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   #this removes the extra break between label and field if error is thrown
   validates :prename, presence: true
   validates :lastname, presence: true
-  validates :username, uniqueness: true, allow_nil: true
+  validates :username, :uniqueness => {:case_sensitive => false}, allow_nil: true
   validates :unit_id, presence: true
   validates_uniqueness_of :email, :allow_nil => true
   validates_with Users_Validator, :on => :create
