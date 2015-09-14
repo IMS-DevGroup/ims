f0f06ec5a902e946be5abcbc04a03b87063c1ac2
djuelich <djuelich@uni-osnabrueck.de>
2015-09-14 13:26:32 +0200

    added current_user language settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,10 +44,13 @@ class ApplicationController < ActionController::Base
 
   #sets localisation (magic)
   private
-  def set_locale
-    I18n.locale = session[:locale] || I18n.default_locale
-    session[:locale] = I18n.locale
-  end
 
+  def set_locale
+    if current_user != nil
+      I18n.locale = current_user.language
+    else
+      I18n.locale = :en
+    end
+  end
 
 end
