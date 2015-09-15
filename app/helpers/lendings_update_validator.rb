ea79fc8620fdba5994ab26e4e3799bc539451e2a
Lena Scholz <lscholz@uos.de>
2015-09-15 16:17:42 +0200

    Changed lendings validation to only look for existing lending on creation, not update

---
+++ app/helpers/lendings_update_validator.rb
@@ -0,0 +1,16 @@
+class Lendings_Update_Validator < ActiveModel::Validator
+
+  def validate(lending)
+    if User.find_by_id(lending.lender_id).nil? then lending.errors[:base] = "Lender does not exist"
+    elsif User.find_by_id(lending.user_id).nil? then lending.errors[:base] = "User does not exist"
+    elsif (@dev = Device.find_by_id(lending.device_id)).nil? then lending.errors[:base] = "Device does not exist"
+    end
+    if !(lending.receiver_id.blank?) or !(lending.receive.blank?) or !(lending.receive_info.blank?)
+      if lending.receiver_id.blank? then lending.errors[:base] = "Receiver cannot be blank when other receive-data exists"
+      elsif lending.receive.blank? then lending.errors[:base] = "Receive cannot be blank when other receive-data exists"
+      elsif User.find_by_id(lending.receiver_id).nil? then lending.errors[:base] = "Receiver does not exist"
+      end
+    end
+
+  end
+end
