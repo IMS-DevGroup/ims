f1176e5303b7754d8e03f342ed35dfec48006869
eossovski <eossovski@uos.de>
2015-09-15 14:58:03 +0200

    added fields to notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -69,6 +69,6 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params.require(:notification).permit(:subject, :info, :checked)
+      params.require(:notification).permit(:subject, :info, :checked, :unit_id, :device_id)
     end
 end
