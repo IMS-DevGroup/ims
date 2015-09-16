957c74874a4373fa8ca26195a226ade816606b31
ramarx <ramarx@cip89.informatik.uni-osnabrueck.de>
2015-09-16 15:04:32 +0200

    error message if server is down bugfix

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -115,7 +115,7 @@ class LendingsController < ApplicationController
   # DELETE /lendings/1.json
   def destroy
     if BossConfig.first.db_state == false
-      flash[:error] = 'Datenbank Status: Im Einsatz, keine keine Änderung mölgich'
+      flash[:error] = (I18n.t "own.errors.db_offline").to_s
       redirect_to "/starts/"
     else
       @lending.destroy
@@ -129,7 +129,7 @@ class LendingsController < ApplicationController
   # GET lendings/1/return
   def return
     if BossConfig.first.db_state == false
-      flash[:error] = 'Datenbank Status: Im Einsatz, keine keine Änderung mölgich'
+      flash[:error] = (I18n.t "own.errors.db_offline").to_s
       redirect_to "/starts/"
     end
   end
@@ -138,7 +138,7 @@ class LendingsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_lending
     if BossConfig.first.db_state == false
-      flash[:error] = 'Datenbank Status: Im Einsatz, keine keine Änderung mölgich'
+      flash[:error] = (I18n.t "own.errors.db_offline").to_s
       redirect_to "/starts/"
     else
       @lending = Lending.find(params[:id])
@@ -148,7 +148,7 @@ class LendingsController < ApplicationController
   # Set all devices for later use in device-selector-coffeescript
   def set_devices
     if BossConfig.first.db_state == false
-      flash[:error] = 'Datenbank Status: Im Einsatz, keine keine Änderung mölgich'
+      flash[:error] = (I18n.t "own.errors.db_offline").to_s
       redirect_to "/starts/"
     else
       #check for set stock
