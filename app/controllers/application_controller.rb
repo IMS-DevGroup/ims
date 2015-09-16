b4a4b60535d17567e9fa040caf23ad8706f241ef
eossovski <eossovski@uos.de>
2015-09-16 14:01:21 +0200

    changed set_local configuration locale set by user prefered language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,9 +47,15 @@ class ApplicationController < ActionController::Base
   #sets localisation (magic)
   private
   def set_locale
-    I18n.locale = session[:locale] || I18n.default_locale
-    session[:locale] = I18n.locale
-    gon.locale = I18n.locale
+    if current_user then
+      I18n.locale = @current_user[:language]
+      session[:locale] = I18n.locale
+      gon.locale = I18n.locale
+    else
+      I18n.locale = session[:locale] || I18n.default_locale
+      session[:locale] = I18n.locale
+      gon.locale = I18n.locale
+    end
   end
 
 
